f5b1f50f08ffc4470fbab354355f85f6bd71eae3
Nameer Nihad PC <nameernihadpc@gmail.com>
2023-10-17 12:28:19 +0530

    http method update

--- src/interface/routes/trainerRoutes.js
+++ src/interface/routes/trainerRoutes.js
@@ -15,7 +15,7 @@ const trainerRoutes = express.Router();
 trainerRoutes.post("/",checkTrainerStatusByEmail, Login);
 trainerRoutes.post("/sentEmail", sentEmails);
 
-trainerRoutes.post('/editProfile',trainerAuthToken,checkUserStatusByUserId,trainerEditProfile)
+trainerRoutes.put('/editProfile',trainerAuthToken,checkUserStatusByUserId,trainerEditProfile)
 
 trainerRoutes.get("/getTrainer", trainerAuthToken,checkUserStatusByUserId, getTrainerById);
 trainerRoutes.get("/getSubscription", trainerAuthToken,checkUserStatusByUserId, getSubscription);
