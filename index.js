9bfd2846db6e2302243dd8b8a6eeed5b76ab2017
Nameer Nihad PC <nameernihadpc@gmail.com>
2023-10-18 16:46:50 +0530

    sample 1

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: ['https://gymease.vercel.app','http://localhost:3000'],
+  origin: ['https://gymease.vercel.app', 'http://localhost:3000'],
+  // Add additional CORS configuration if needed
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
 }));
 
 
